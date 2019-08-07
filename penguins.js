1c477568e0d09b92b0fc7ae733ee20600decee18
Alex Theg <charles.theg@gmail.com>
2019-08-06 23:33:33 -0700

    clean up terrible code

--- penguins.js
+++ penguins.js
@@ -1,97 +1,82 @@
 // controller
 var PenguinController = function(penguinView, penguinModel) {
-  this.penguinView = penguinView;
-  this.penguinModel = penguinModel;
-}
 
-PenguinController.prototype.initialize = function initialize() {
-  this.penguinView.onClickGetPenguin = this.onClickGetPenguin.bind(this)
-};
+  this.onClickGetPenguin = function(e) {
+    // `penguinIndex` stored as "data-penguin-index" attr on the HTML
+    // button, and retrieved with `dataset` from the DOMStringMap
+    var index = parseInt(e.currentTarget.dataset.penguinIndex, 10);
+    penguinModel.getPenguin(index, this.showPenguin.bind(this));
+  }
 
-PenguinController.prototype.onClickGetPenguin = function onClickGetPenguin(e) {
-  var target = e.currentTarget;
-  var index = parseInt(target.dataset.penguinIndex, 10);
+  this.showPenguin = function(penguinModelData) {
+    // penguinViewModel calculate previousIndex and nextIndex, adds them
+    // to the object, and render it
+    var penguinViewModel = {
+      name: penguinModelData.name,
+      imageUrl: penguinModelData.imageUrl,
+      size: penguinModelData.size,
+      favoriteFood: penguinModelData.favoriteFood
+    };
 
-  this.penguinModel.getPenguin(index, this.showPenguin.bind(this));
-}
+    penguinViewModel.previousIndex = penguinModelData.index - 1;
+    penguinViewModel.nextIndex = penguinModelData.index + 1;
+    if (penguinModelData.index === 0) {
+      penguinViewModel.previousIndex = penguinModelData.count - 1;
+    }
+    if (penguinModelData.index === penguinModelData.count - 1) {
+      penguinViewModel.nextIndex = 0;
+    }
 
-PenguinController.prototype.showPenguin = function showPenguin(penguinModelData) {
-  var penguinViewModel = {
-    name: penguinModelData.name,
-    imageUrl: penguinModelData.imageUrl,
-    size: penguinModelData.size,
-    favoriteFood: penguinModelData.favoriteFood
+    penguinView.render(penguinViewModel);
   };
 
-  penguinViewModel.previousIndex = penguinModelData.index - 1;
-  penguinViewModel.nextIndex = penguinModelData.index + 1;
-  if (penguinModelData.index === 0) {
-    penguinViewModel.previousIndex = penguinModelData.count - 1;
-  }
-  if (penguinModelData.index === penguinModelData.count - 1) {
-    penguinViewModel.nextIndex = 0;
-  }
+  penguinView.onClickGetPenguin = this.onClickGetPenguin.bind(this)
+}
 
-  this.penguinView.render(penguinViewModel);
-};
+var PenguinView = function(rootEl) {
 
-var PenguinView = function PenguinView(element) {
-  this.element = element;
+  this.render = function(viewModel) {
+    rootEl.innerHTML = '<h3>' + viewModel.name + '</h3>' +
+      '<img class="penguin-image" src="' + viewModel.imageUrl +
+        '" alt="' + viewModel.name + '"/>' +
+        '<p><b>Size: </b>' + viewModel.size + '</p>' +
+        '<p><b>Favorite food: </b>' + viewModel.favoriteFood + '</p>' +
+        '<a id="previousButton" class="previous button" href="javascript:void(0);"' +
+          'data-penguin-index="' + viewModel.previousIndex + '">Previous</a>' +
+        '<a id="nextButton" class="next button" href="javascript:void(0);"' +
+          'data-penguin-index="' + viewModel.nextIndex + '">Next</a>'
 
-  this.onClickGetPenguin = null;
-};
+    var previousButton = rootEl.querySelector('#previousButton');
+    previousButton.addEventListener('click', this.onClickGetPenguin);
 
-// view
-PenguinView.prototype.render = function render(viewModel) {
-  this.element.innerHTML = '<h3>' + viewModel.name + '</h3>' +
-    '<img class="penguin-image" src="' + viewModel.imageUrl +
-      '" alt="' + viewModel.name + '"/>' +
-      '<p><b>Size: </b>' + viewModel.size + '</p>' +
-      '<p><b>Favorite food: </b>' + viewModel.favoriteFood + '</p>' +
-      '<a id="previousPenguin" class="previous button" href="javascript:void(0);"' +
-        'data-penguin-index="' + viewModel.previousIndex + '">Previous</a>' +
-      '<a id="nextPenguin" class="next button" href="javascript:void(0);"' +
-        'data-penguin-index="' + viewModel.nextIndex + '">Next</a>'
-
-  this.previousIndex = viewModel.previousIndex;
-  this.nextIndex = viewModel.nextIndex;
-
-  var previousPenguin = this.element.querySelector('#previousPenguin');
-  previousPenguin.addEventListener('click', this.onClickGetPenguin);
-
-  var nextPenguin = this.element.querySelector('#nextPenguin');
-  nextPenguin.addEventListener('click', this.onClickGetPenguin);
-  nextPenguin.focus();
+    var nextButton = rootEl.querySelector('#nextButton');
+    nextButton.addEventListener('click', this.onClickGetPenguin);
+  };
 };
 
 // model
-var PenguinModel = function PenguinModel(XMLHttpRequest) {
-  this.XMLHttpRequest = XMLHttpRequest;
-};
+var PenguinModel = function PenguinModel() {
+  this.getPenguin = function getPenguin(index, fn) {
+    var oReq = new XMLHttpRequest();
 
-PenguinModel.prototype.getPenguin = function getPenguin(index, fn) {
-  var oReq = new this.XMLHttpRequest();
+    oReq.onload = function onLoad(e) {
+      var ajaxResponse = JSON.parse(e.currentTarget.responseText);
+      var penguin = ajaxResponse[index];
 
-  oReq.onload = function onLoad(e) {
-    var ajaxResponse = JSON.parse(e.currentTarget.responseText);
-    var penguin = ajaxResponse[index];
+      penguin.index = index;
+      penguin.count = ajaxResponse.length;
 
-    penguin.index = index;
-    penguin.count = ajaxResponse.length;
+      fn(penguin);
+    };
 
-    fn(penguin);
+    oReq.open('GET', 'https://codepen.io/beautifulcoder/pen/vmOOLr.js', true);
+    oReq.send();
   };
-
-  oReq.open('GET', 'https://codepen.io/beautifulcoder/pen/vmOOLr.js', true);
-  oReq.send();
 };
 
-var penguinModel = new PenguinModel(XMLHttpRequest);
-
-var targetElement = document.getElementById('root');
-var penguinView = new PenguinView(targetElement);
+var penguinModel = new PenguinModel;
+var penguinView = new PenguinView(document.getElementById('root'));
 var controller = new PenguinController(penguinView, penguinModel)
 
-controller.initialize();
-
+// simulate an onClickGetPenguin with a mock `e.currentTarget.dataset.penguinIndex` event to get the first penguin
 controller.onClickGetPenguin({ currentTarget: { dataset: { penguinIndex: 0 }}});
